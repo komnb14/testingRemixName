60534b447b7b28e598a58921d3dec60495f63e8e
komnb14 <komnb14@gmail.com>
2023-02-20 03:29:31 +0900

    first

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,21 +2,23 @@ import {Link, useLoaderData} from "@remix-run/react";
 import {json} from "@remix-run/cloudflare";
 
 export const loader = async () => {
-  const uuid = await fetch("https://uuid.rocks/plain").then(
-      async (response) => await response.text()
-  );
+    const uuid = await fetch("https://uuid.rocks/plain").then(
+        async (response) => await response.text()
+    );
 
-  return json({ ok: true,uuid });
+    return json({ok: true, uuid});
 };
 export default function Index() {
-  const data = useLoaderData<typeof loader>()
+    const data = useLoaderData<typeof loader>()
 
 
-  return (
-    <div>
-      <div>테스팅 사이트</div>
+    return (
+        <div>
+            <div>테스팅 사이트</div>
 
-      <Link to={`/${data.uuid}`}>{data.uuid}</Link>
-    </div>
-  );
+            <Link to={`/${data.uuid}`}>
+                <button>이동</button>
+            </Link>
+        </div>
+    );
 }
